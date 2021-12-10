7bb23744ae13e62b756ca1e9b2f87d368425e18c
The_Pwnish3r <email_id>
2021-12-10 04:38:27 +0200

    tokens loop

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,74 @@
-use std::fs;
 use std::env;
+use std::fs;
 use std::panic;
-enum TokenKind{
+enum TokenKind {
+    Identifier,
 
-    
+    Assign,
 
+    Let,
 
-
+    String,
 }
 
-struct Token{
-
-
-    token_type:TokenKind,
+struct Token {
+    kind: TokenKind,
 
     literal: String,
-
-
-
 }
 
-
+impl Token {
+    pub fn new(kind: TokenKind, literal: String) -> Self {
+        Self { kind, literal }
+    }
+}
 
 #[derive(Debug)]
 struct Lexer {
-
-
-    source : Vec<char>,
-    counter : usize,
-
+    source: Vec<char>,
+    counter: usize,
 }
 
 impl Lexer {
+    pub fn new(contents: String) -> Self {
+        Self {
+            source: contents.chars().collect(),
 
-    pub fn new(contents:String)->Self{
-
-        Self{
-            source:contents.chars().collect(),
-
-            counter:0,
-    
+            counter: 0,
         }
-
-
     }
 
-    pub fn lex(&mut self){
+    pub fn lex(&mut self) {
+        let tokens: Vec<Token> = Vec::new();
+
+        while self.source.len() > self.counter {
 
 
-        println!("{}", self.contents);
+            
 
 
+
+        }
     }
-
 }
 fn main() {
-
     let maybe_file = env::args().nth(1);
 
-    let file= if let Some(f) =  maybe_file{
-
-
-        f 
-
-
-    }else {
-        
-        panic!("Expected a file.")        
-
-
+    let file = if let Some(f) = maybe_file {
+        f
+    } else {
+        panic!("Expected a file.")
     };
 
+    let maybe_contents = fs::read_to_string(file);
 
-    let maybe_contents  = fs::read_to_string(file);   
-
-
-    let contents = if maybe_contents.is_ok(){
-
-
+    let contents = if maybe_contents.is_ok() {
         maybe_contents.unwrap()
-        
-    }else{
-
-
+    } else {
         panic!("couldn't open file for reading ");
-
     };
-    
-    let mut lexer = Lexer::new(contents);   
-    
+
+    let mut lexer = Lexer::new(contents);
 
     lexer.lex();
-
 }
